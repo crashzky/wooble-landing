3e32ed08f21f426eb7dbf0c4dd6e1be40102ff7d
Kirill Osintsev <osintsev.2000@inbox.ru>
2022-04-17 22:18:25 +0500

    added yandex metrika

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,11 +6,30 @@ class CustomDocument extends Document {
 		return { ...initialProps };
 	}
 
+	ym() {
+		return (
+		  '<script src=\'https://mc.yandex.ru/metrika/watch.js\' type=\'text/javascript\'></script>' +
+		  '<script type=\'text/javascript\'>' +
+				'try {' +
+					  'var yaCounter88408407 = new Ya.Metrika({' +
+					  'id:88408407,' +
+					  'clickmap:true,' +
+					  'trackLinks:true,' +
+					  'accurateTrackBounce:true,' +
+					  'webvisor:true,' +
+					  'trackHash:true' +
+					'});' +
+				'} catch(e) { }' +
+		  '</script>'
+		);
+	};
+
 	render(): JSX.Element {
 		return (
 			<Html lang='ru-RU'>
 				<Head />
 				<body>
+					<div dangerouslySetInnerHTML={{ __html: this.ym() }} />
 					<Main />
 					<NextScript />
 				</body>
