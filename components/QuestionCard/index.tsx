5463eb5b098c9c22bfbaec4f8e95df0d2c990f18
Кирилл Осинцев <osintsev.2000@inbox.ru>
2022-07-29 01:54:40 +0500

    fix

--- components/QuestionCard/index.tsx
+++ components/QuestionCard/index.tsx
@@ -1,8 +1,8 @@
 import Props from './QuestionCard.props';
+import { useState } from 'react';
 
 import PlusIcon from '../../assets/plus.svg';
 import MinusIcon from '../../assets/minus.svg';
-import { useState } from 'react';
 
 const QuestionCard: React.FC<Props> = ({ className = '', label, content, onClick = () => {}, ...props }) => {
 	const [isOpen, setIsOpen] = useState(false);
@@ -17,7 +17,7 @@ const QuestionCard: React.FC<Props> = ({ className = '', label, content, onClick
 			{...props}
 		>
 			<div className='flex justify-between flex-row-reverse lg:flex-row items-center'>
-				<h3 className='font-bold text-lg text-left ml-5 lg:ml-auto'>
+				<h3 className='font-bold text-lg text-left lg:w-full ml-5 lg:ml-auto'>
 					{label}
 				</h3>
 				{isOpen ? (
