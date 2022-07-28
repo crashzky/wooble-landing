307bc71e26efc21bd20b1bd441396b76f3baa26b
Кирилл Осинцев <osintsev.2000@inbox.ru>
2022-07-29 02:01:31 +0500

    SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,11 +10,36 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
 				<title>
 					Wooble - студия разработки полного цикла
 				</title>
+				<meta httpEquiv='Content-Security-Policy' content='upgrade-insecure-requests' /> 
 				<link rel='icon' href='/favicon.ico' type='image/x-icon' />
 				<meta
 					name='description'
 					content='Мы небольшая команда, создающая IT-решения для бизнеса разной направленности и сложности.
 					Разрабатываем сайты, приложения, дизайн и  предоставляем услуги по доработке уже существующих продуктов.' />
+				<meta
+					name='keywords'
+					content='сайты, сайты под ключ, сайт под ключ, разработка, программирование, программист' />
+				<meta property='og:type' content='website' />
+				<meta property='og:url' content='https://wooble.space/' />
+				<meta property='og:title' content='Wooble - студия разработки полного цикла' />
+				<meta
+					property='og:description'
+					content='Мы небольшая команда, создающая IT-решения для бизнеса разной направленности и сложности.
+					Разрабатываем сайты, приложения, дизайн и  предоставляем услуги по доработке уже существующих продуктов.' />
+				<meta
+					property='og:image'
+					content='/og_image.png' />
+					
+				<meta property='twitter:card' content='summary_large_image' />
+				<meta property='twitter:url' content='https://wooble.space/' />
+				<meta property='twitter:title' content='Wooble - студия разработки полного цикла' />
+				<meta
+					property='twitter:description'
+					content='Мы небольшая команда, создающая IT-решения для бизнеса разной направленности и сложности.
+					Разрабатываем сайты, приложения, дизайн и  предоставляем услуги по доработке уже существующих продуктов.' />
+				<meta
+					property='twitter:image'
+					content='/og_image.png' />
 			</Head>
 			<Component {...pageProps} />
 		</>
